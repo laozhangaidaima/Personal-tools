d48edc6a10a0b825411f5dac6342f9ccf85ffecf
laozhangaidaima <751818302zh@gmail.com>
2020-04-20 20:02:15 +0800

    vscode自己模拟数据

---
+++ mock/1.js
@@ -0,0 +1,3 @@
+module.exports = {
+    '/api/1':{"code":100,"msg":"执行成功","content":[{"all":200.0,"use":0.0,"per":0.0,"name":"新牌坊"},{"all":3600.0,"use":0.0,"per":0.0,"name":"回兴I"},{"all":30.0,"use":0.0,"per":0.0,"name":"123"},{"all":600.0,"use":0.0,"per":0.0,"name":"123"}]}
+}
