e85cc91e80f6fc3b00d4ab7b0ce2ebfbeedc6941
张寒 <zhanghan@shanglv51.com>
2020-09-08 09:25:06 +0800

    'python更新'

---
+++ autohotkey/常用短语-重要.ahk
@@ -0,0 +1,50 @@
+
+; O  确定键 输入的空格、回车是不生效的
+:O:u11::15928446337
+:O:u12::ly123456
+:O:u21::13438067470
+:O:u22::123456
+:O:u31::18628309200
+:O:u32::309200
+:O:u41::18190165596
+:O:u42::165596F
+
+:O:git1::git add .
+:O:git2::git commit -m '' --no-verify
+:O:git3::git push
+
+:O:npm1::npm run dev
+:O:npm2::npm run serve
+
+:O:f1::document.body.contentEditable="true"
+
+
+
+
+
+
+
+; 鼠标滑轮上下翻页速度调整
+; 默认速度3左右合适
+WheelDown::
+    MouseClick, WheelDown, , , 3, 0, D, R
+Return
+
+WheelUp::
+    MouseClick, WheelUp, , , 3, 0, D, R
+Return
+
+; 按下PrintScreen 可切换菜单
+; 按下Enter 选择菜单并结束
+; 一定要按下Enter键
+PrintScreen::
+    {
+        Send, {Alt Down}
+        Send, {Tab Down}
+        keywait, Enter, D
+        Send, {Alt Up}
+        Send, {Tab Up}
+    }
+Return
+
+
--- 测试玩耍/python玩耍/test.py
+++ 测试玩耍/python玩耍/test.py
@@ -1,7 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+
+import os
+import time
 # 发送请求
+
+
 def axios(Authorization, ind=1):
     url = "http://nwgateway.shanglv51.com/domestichotel/ck/app/hotels?locationID=&address=&arriveDate=2020-09-07&bedType=0&brandName=&breakfast=0&cancelRule=0&companyID=1901111714441860c0400232327&confirmation=0&departureDate=2020-09-08&distance=&facility=0&keyWord=&pageSize=20&paymentType=0&price=&searchType=2&sort=&star=&theme=&rating=&travelCharge=0&type=1&lat=30.57613912400076&lng=104.07056297370212&cityName=%E6%88%90%E9%83%BD&locationType=0&searchId=&pageIndex=" + \
         str(1)
@@ -15,7 +20,7 @@ pass
 
 
 # 处理数据
-Authorization = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxOTAxMTAwOTQ4MzY3ODEwYzEyMDAyMzcyMzMiLCJ1c2VySUQiOiIxOTAxMTAwOTI4MzY3ODEwYzEyMDAyMzcyMzM4IiwiY29tcGFueUlEIjoiMTkwNDExMTcxNDQ0MTg2MGMwNDAwMjMyMzI3MyIsInVzZXJBY2NvdW50IjoiMTU5Mjg0MDYzMzc2IiwicGFydG5lckNvZGUiOiIwMSIsImlhdCI6MTU5OTQ0NTkwOCwiZXhwIjoxNTk5NDQ3MTA4fQ.jsCzNQO1NEn4WEkzDSNRaFDKG9wsh-ql3W9dRk36RmI"
+Authorization = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxOTAxMTAwOTQ4MzY3ODEwYzEyMDAyMzcyMzMiLCJ1c2VySUQiOiIxOTAxMTAwNjQ4MzY3ODEwYzEyMDAyMzcyMzM3IiwiY29tcGFueUlEIjoiMTkwMTExMTc5NDQ0MTg2MGMwNDAwMjMyMzI3OCIsInVzZXJBY2NvdW50IjoiMTU5Mjg0MDYzMzc2IiwicGFydG5lckNvZGUiOiIwMSIsImlhdCI6MTU5OTQ1OTA1NSwiZXhwIjoxNTk5NDYwMjU1fQ.61sMkh-bdTK8SLBMV7NqhurVvDvVHalCnhhZAbacnP8"
 ind = 1
 res = axios(Authorization, ind)
 json_str = json.loads(res)
@@ -24,9 +29,28 @@ data = json_str['data']
 imgs = []
 for item in data:
     imgs.append(item['thumbnailId'])
-print("imgs",imgs)
+print("imgs", imgs)
 
 # 保存数据
-fp = open("a.json", 'w', encoding='utf-8', errors='ignore')
-json_save = json.dumps(imgs, ensure_ascii=False,indent=4)
-fp.write(json_save)
+# fp = open("a.json", 'w', encoding='utf-8', errors='ignore')
+# json_save = json.dumps(imgs, ensure_ascii=False,indent=4)
+# fp.write(json_save)
+
+
+# 下载图片
+# imgs
+
+# with open("/tmp/foo.txt") as file:
+#     data = file.read()
+
+#保存图片，思路：将所有的图片保存在本地的一个文件夹下，用图片的url链接的后缀名来命名
+dir_name = 'teacherImage'    #设置文件夹的名字
+if not os.path.exists(dir_name):     #os模块判断并创建
+    os.mkdir(dir_name)
+
+for img_url in imgs:
+    time.sleep(1)   #设置间隔时间，防止把网页爬崩
+    picture_name = img_url.split('/')[-1]     #提取图片url后缀
+    reponse = requests.get(img_url)
+    with open(dir_name+'/'+picture_name,'wb') as f:  # 还有遍历的作用
+        f.write(reponse.content)
